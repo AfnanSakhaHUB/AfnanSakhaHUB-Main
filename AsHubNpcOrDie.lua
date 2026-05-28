920e9546cd4c0a97f7a08ea5cfc337b365ea5184
AfnanSakhaHUB <afnanwilliam67@gmail.com>
2026-05-28 11:58:11 +0700

    Update AsHubNpcOrDie.lua

--- AsHubNpcOrDie.lua
+++ AsHubNpcOrDie.lua
@@ -687,7 +687,7 @@ elseif name == "Auto Run when Sherrif is near" then
 		
 		-- Mengatur Ukuran dan Posisi tepat di atas tombol lari asli (sebelah kiri tombol lompat)
 		overlayButton.Size = UDim2.new(0, 65, 0, 65) 
-		overlayButton.Position = UDim2.new(1, -150, 1, -95) 
+		overlayButton.Position = UDim2.new(1, -145, 1, -55) 
 		overlayButton.AnchorPoint = Vector2.new(0.5, 0.5)
 		
 		-- PENGATURAN AWAL: Tombol 100% Tidak Terlihat (Transparansi = 1)
